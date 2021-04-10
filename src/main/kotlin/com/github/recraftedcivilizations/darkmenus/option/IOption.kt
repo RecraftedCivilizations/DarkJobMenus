9168da77b1f11b7a79fa924bee2569089802c876
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-04-10 20:09:34 +0200

    feat: Interface that represents an generic Option that gets called from a menu

---
+++ src/main/kotlin/com/github/recraftedcivilizations/darkmenus/option/IOption.kt
@@ -0,0 +1,26 @@
+package com.github.recraftedcivilizations.darkmenus.option
+
+import com.github.recraftedcivilizations.darkcitizens.dPlayer.DPlayer
+import org.bukkit.entity.Player
+
+/**
+ * @author DarkVanityOfLight
+ */
+
+/**
+ * Represents an generic option that can be called from a menu
+ */
+interface IOption {
+
+    /**
+     * Defines what happens when the option gets executed
+     * @param sender The Player that called the option
+     */
+    fun onCall(sender: Player)
+
+    /**
+     * Defines what happens when the option gets executed
+     * @param sender The Player that called the option
+     */
+    fun onCall(sender: DPlayer)
+}
