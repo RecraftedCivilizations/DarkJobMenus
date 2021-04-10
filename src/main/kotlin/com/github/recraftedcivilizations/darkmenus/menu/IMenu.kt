c30560a0550b21d6e987140af367cb284c165630
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-04-10 20:04:07 +0200

    feat: Interface that represents a generic menu

---
+++ src/main/kotlin/com/github/recraftedcivilizations/darkmenus/menu/IMenu.kt
@@ -0,0 +1,33 @@
+package com.github.recraftedcivilizations.darkmenus.menu
+
+import com.github.recraftedcivilizations.darkcitizens.dPlayer.DPlayer
+import com.github.recraftedcivilizations.darkmenus.option.IOption
+import org.bukkit.entity.Player
+
+/**
+ * @author DarkVanityOfLight
+ */
+
+/**
+ * Represents an menu that contains actions/options that can be executed
+ * @property options All options that can be executed from this menu
+ * @property name The name of this menu
+ */
+interface IMenu {
+    val options: List<IOption>
+    val name: String
+
+    /**
+     * Execute an specific option
+     * @param player The player that executed the option
+     * @param position The position of the option
+     */
+    fun executeOption(player: Player, position: Int): Unit
+
+    /**
+     * Execute an specific option
+     * @param player The player that executed the option
+     * @param position The position of the option
+     */
+    fun executeOption(sender: DPlayer, position: Int): Unit
+}
