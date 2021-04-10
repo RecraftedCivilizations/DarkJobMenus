d3f6526e1cdfaedb64713ad5b0d78b1523bffba6
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-04-10 19:59:18 +0200

    refractor: darkjobmenus to darkmenus

---
+++ src/main/kotlin/com/github/recraftedcivilizations/darkmenus/Main.kt
@@ -0,0 +1,11 @@
+package com.github.recraftedcivilizations.darkmenus
+
+import com.github.darkvanityoflight.recraftedcore.ARecraftedPlugin
+import com.github.recraftedcivilizations.darkcitizens.DarkCitizens
+
+class Main : ARecraftedPlugin(){
+
+    override fun onEnable() {
+        val jobManager = DarkCitizens.jobManager
+    }
+}
