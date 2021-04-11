572fe8042cf30e33da3136dc20ce65475f200153
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-04-11 19:58:06 +0200

    chore: Docs

--- src/main/kotlin/com/github/recraftedcivilizations/darkmenus/menu/menus/MenuFactory.kt
+++ src/main/kotlin/com/github/recraftedcivilizations/darkmenus/menu/menus/MenuFactory.kt
@@ -5,6 +5,13 @@ import com.github.recraftedcivilizations.darkmenus.BukkitWrapper
 import com.github.recraftedcivilizations.darkmenus.menu.IMenu
 import com.github.recraftedcivilizations.darkmenus.option.IOption
 
+/**
+ * @author DarkVanityOfLight
+ */
+
+/**
+ * Create new menus here
+ */
 object MenuFactory {
     private var bukkitWrapper = BukkitWrapper()
 
@@ -12,6 +19,14 @@ object MenuFactory {
         this.bukkitWrapper = BukkitWrapper()
     }
 
+    /**
+     * Create a new menu here
+     * @param name The name of the menu
+     * @param isGui Should the menu be a GUI
+     * @param isJobSpecific Should the menu be specific to a job
+     * @param options All options this menu should contain
+     * @param job Only has to be provided if you want a job specific menu
+     */
     fun newMenu(name: String, isGui: Boolean, isJobSpecific: Boolean, options: List<IOption>, job: IJob? = null): IMenu{
 
         if (isGui && isJobSpecific){
