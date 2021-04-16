f6e62ae25fabe448c3b48080462a9b07888fd6c3
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-04-16 10:38:22 +0200

    fix: Add the specific to property to every Menu

--- src/main/kotlin/com/github/recraftedcivilizations/darkmenus/menu/IMenu.kt
+++ src/main/kotlin/com/github/recraftedcivilizations/darkmenus/menu/IMenu.kt
@@ -1,6 +1,7 @@
 package com.github.recraftedcivilizations.darkmenus.menu
 
 import com.github.recraftedcivilizations.darkcitizens.dPlayer.DPlayer
+import com.github.recraftedcivilizations.darkmenus.menu.menus.SpecificTo
 import com.github.recraftedcivilizations.darkmenus.option.IOption
 import org.bukkit.entity.Player
 
@@ -16,6 +17,7 @@ import org.bukkit.entity.Player
 interface IMenu {
     val options: List<IOption>
     val name: String
+    val specificTo: SpecificTo
 
     /**
      * Execute an specific option
