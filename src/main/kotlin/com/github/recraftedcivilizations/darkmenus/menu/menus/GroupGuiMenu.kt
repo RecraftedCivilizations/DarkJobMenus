3d0411efac6c573ea77ded51c65b1f5b45d48d9c
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-04-18 16:01:01 +0200

    feat: Create an Implementation for a menu that is a gui and specific to a group

---
+++ src/main/kotlin/com/github/recraftedcivilizations/darkmenus/menu/menus/GroupGuiMenu.kt
@@ -0,0 +1,75 @@
+package com.github.recraftedcivilizations.darkmenus.menu.menus
+
+import com.github.darkvanityoflight.recraftedcore.gui.InventoryGUI
+import com.github.darkvanityoflight.recraftedcore.gui.elements.CloseButtonFactory
+import com.github.recraftedcivilizations.darkcitizens.dPlayer.DPlayer
+import com.github.recraftedcivilizations.darkcitizens.groups.Group
+import com.github.recraftedcivilizations.darkmenus.BukkitWrapper
+import com.github.recraftedcivilizations.darkmenus.menu.GUIMenu
+import com.github.recraftedcivilizations.darkmenus.menu.IGroupMenu
+import com.github.recraftedcivilizations.darkmenus.option.GUIOption
+import com.github.recraftedcivilizations.darkmenus.option.IOption
+import org.bukkit.entity.Player
+
+/**
+ * Represents a GUI menu for a specific group
+ * @param name The name of the menu
+ * @param options All options that can be executed with this menu
+ * @param group The group this menu belongs too
+ * @param bukkitWrapper BukkitWrapper for debugging only
+ */
+class GroupGuiMenu(override val name: String, options: List<IOption>, override val group: Group, private val bukkitWrapper: BukkitWrapper = BukkitWrapper()) : IGroupMenu, GUIMenu {
+    private val gui: InventoryGUI
+    override val options: List<GUIOption>
+
+    init {
+
+        var opts = emptyList<GUIOption>().toMutableList()
+
+        for (option in options){
+            if (option is GUIOption){
+                opts.add(option)
+            }
+        }
+
+        this.options = opts
+
+        // Make the number of options dividable by 9
+        // +1 Because of the Close button
+        var invSize = this.options.size + 1
+        if(invSize % 9 != 0){
+            invSize += (9 - (invSize % 9))
+        }
+        gui = InventoryGUI(invSize, name)
+
+        for (item in this.options){
+            gui.addItem(item.displayItem)
+        }
+
+        gui.setSlot(CloseButtonFactory.getCloseButton(), invSize-1)
+    }
+    /**
+     * Display the GUI to a sender
+     */
+    override fun display(player: Player) {
+        gui.show(player)
+    }
+
+    /**
+     * Execute an specific option
+     * @param player The player that executed the option
+     * @param position The position of the option
+     */
+    override fun executeOption(player: Player, position: Int) {
+        options[position].onCall(player)
+    }
+
+    /**
+     * Execute an specific option
+     * @param sender The player that executed the option
+     * @param position The position of the option
+     */
+    override fun executeOption(sender: DPlayer, position: Int) {
+        executeOption(bukkitWrapper.getPlayer(sender.uuid)!!, position)
+    }
+}
